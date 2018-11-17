f47cab47103cab06d698df094a84e40ea9189ff1
Krzesimir Nowak <qdlacz@gmail.com>
2018-11-18 00:23:56 +0100

    foo

--- ggp/test/type-test.cc
+++ ggp/test/type-test.cc
@@ -4,8 +4,28 @@
 
 using namespace Ggp::Lib;
 
+namespace
+{
+
+auto tfs(char const* str) -> Types
+{
+  auto v {VariantFormat::from_string (str)};
+
+  REQUIRE(v.has_value());
+
+  // TODO
+}
+
+} // anonymous namespace
+
 TEST_CASE ("format to types", "[type]")
 {
+  SECTION ("basic types")
+  {
+    using namespace std::string_literals;
+
+    CHECK (tfs ("b") == Types{{{PlainType{{Integral{"gboolean"s, {}, {"gint"s, "int"s}, sizeof(int), Signedness::Any}}}}}, {{NullablePointer{PlainType{{Integral{"gboolean"s, {}, {"gint"s, "int"s}, sizeof(int), Signedness::Any}}}}}}});
+  }
   // TODO: See "gvariant format strings" in devhelp for information
   // about expected types for a format
 }
