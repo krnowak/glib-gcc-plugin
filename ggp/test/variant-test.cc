bf194a340526e8b003db31887c1da7f156ad82d8
Krzesimir Nowak <qdlacz@gmail.com>
2019-01-14 22:20:51 +0100

    foo

--- ggp/test/variant-test.cc
+++ ggp/test/variant-test.cc
@@ -16,8 +16,6 @@
  * gcc-glib-plugin. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
-
 #include "ggp/test/generated/variant.hh"
 #include "ggp/test/test-print.hh"
 
@@ -55,43 +53,53 @@ basict (T const& t)
   return {Leaf::Basic {{t}}};
 }
 
+template <typename T>
+VariantType
+stringt (T const& t)
+{
+  return {Leaf::StringType {{t}}};
+}
+
 auto none_t {std::optional<VariantType> {}};
 
 } // anonymous namespace
 
+// TODO: coverage
 TEST_CASE ("Variant types are parsed", "[variant-types]")
 {
   SECTION ("basic types")
   {
-    CHECK (vtfs ("b") == opt (basict (Leaf::Bool {})));
-    CHECK (vtfs ("y") == opt (basict (Leaf::Byte {})));
-    CHECK (vtfs ("n") == opt (basict (Leaf::I16 {})));
-    CHECK (vtfs ("q") == opt (basict (Leaf::U16 {})));
-    CHECK (vtfs ("i") == opt (basict (Leaf::I32 {})));
-    CHECK (vtfs ("u") == opt (basict (Leaf::U32 {})));
-    CHECK (vtfs ("x") == opt (basict (Leaf::I64 {})));
-    CHECK (vtfs ("t") == opt (basict (Leaf::U64 {})));
-    CHECK (vtfs ("h") == opt (basict (Leaf::Handle {})));
-    CHECK (vtfs ("d") == opt (basict (Leaf::Double {})));
-    CHECK (vtfs ("s") == opt (basict (Leaf::String {})));
-    CHECK (vtfs ("o") == opt (basict (Leaf::ObjectPath {})));
-    CHECK (vtfs ("g") == opt (basict (Leaf::Signature {})));
+    CHECK (vtfs ("b") == opt (basict (Leaf::bool_)));
+    CHECK (vtfs ("y") == opt (basict (Leaf::byte_)));
+    CHECK (vtfs ("n") == opt (basict (Leaf::i16)));
+    CHECK (vtfs ("q") == opt (basict (Leaf::u16)));
+    CHECK (vtfs ("i") == opt (basict (Leaf::i32)));
+    CHECK (vtfs ("u") == opt (basict (Leaf::u32)));
+    CHECK (vtfs ("x") == opt (basict (Leaf::i64)));
+    CHECK (vtfs ("t") == opt (basict (Leaf::u64)));
+    CHECK (vtfs ("h") == opt (basict (Leaf::handle)));
+    CHECK (vtfs ("d") == opt (basict (Leaf::double_)));
+  }
 
-    CHECK (vtfs ("k") == none_t);
+  SECTION ("string types")
+  {
+    CHECK (vtfs ("s") == opt (stringt (Leaf::string_)));
+    CHECK (vtfs ("o") == opt (stringt (Leaf::object_path)));
+    CHECK (vtfs ("g") == opt (stringt (Leaf::signature)));
   }
 
   SECTION ("other leaf types")
   {
-    CHECK (vtfs ("v") == opt (VariantType {{Leaf::Variant {}}}));
-    CHECK (vtfs ("*") == opt (VariantType {{Leaf::AnyType {}}}));
-    CHECK (vtfs ("r") == opt (VariantType {{Leaf::AnyTuple {}}}));
-    CHECK (vtfs ("?") == opt (VariantType {{Leaf::AnyBasic {}}}));
+    CHECK (vtfs ("v") == opt (VariantType {{Leaf::variant}}));
+    CHECK (vtfs ("*") == opt (VariantType {{Leaf::any_type}}));
+    CHECK (vtfs ("r") == opt (VariantType {{Leaf::any_tuple}}));
+    CHECK (vtfs ("?") == opt (VariantType {{Leaf::any_basic}}));
   }
 
   SECTION ("array types")
   {
-    CHECK (vtfs ("as") == opt (VariantType {{VT::Array {{basict (Leaf::String {})}}}}));
-    CHECK (vtfs ("aas") == opt (VariantType {{VT::Array {{VariantType {{VT::Array {{basict (Leaf::String {})}}}}}}}}));
+    CHECK (vtfs ("as") == opt (VariantType {{VT::Array {{stringt (Leaf::string_)}}}}));
+    CHECK (vtfs ("aas") == opt (VariantType {{VT::Array {{VariantType {{VT::Array {{stringt (Leaf::string_)}}}}}}}}));
 
     CHECK (vtfs ("a") == none_t);
     CHECK (vtfs ("aa") == none_t);
@@ -99,10 +107,10 @@ TEST_CASE ("Variant types are parsed", "[variant-types]")
 
   SECTION ("maybe types")
   {
-    CHECK (vtfs ("ms") == opt (VariantType {{VT::Maybe {{basict (Leaf::String {})}}}}));
-    CHECK (vtfs ("mms") == opt (VariantType {{VT::Maybe {{VariantType {{VT::Maybe {{basict (Leaf::String {})}}}}}}}}));
-    CHECK (vtfs ("mas") == opt (VariantType {{VT::Maybe {{VariantType {{VT::Array {{basict (Leaf::String {})}}}}}}}}));
-    CHECK (vtfs ("ams") == opt (VariantType {{VT::Array {{VariantType {{VT::Maybe {{basict (Leaf::String {})}}}}}}}}));
+    CHECK (vtfs ("ms") == opt (VariantType {{VT::Maybe {{stringt (Leaf::string_)}}}}));
+    CHECK (vtfs ("mms") == opt (VariantType {{VT::Maybe {{VariantType {{VT::Maybe {{stringt (Leaf::string_)}}}}}}}}));
+    CHECK (vtfs ("mas") == opt (VariantType {{VT::Maybe {{VariantType {{VT::Array {{stringt (Leaf::string_)}}}}}}}}));
+    CHECK (vtfs ("ams") == opt (VariantType {{VT::Array {{VariantType {{VT::Maybe {{stringt (Leaf::string_)}}}}}}}}));
 
     CHECK (vtfs ("m") == none_t);
     CHECK (vtfs ("mm") == none_t);
@@ -111,9 +119,9 @@ TEST_CASE ("Variant types are parsed", "[variant-types]")
   SECTION ("tuple types")
   {
     CHECK (vtfs ("()") == opt (VariantType {{VT::Tuple {{}}}}));
-    CHECK (vtfs ("(s)") == opt (VariantType {{VT::Tuple {{basict (Leaf::String {})}}}}));
-    CHECK (vtfs ("(ss)") == opt (VariantType {{VT::Tuple {{basict (Leaf::String {}), basict (Leaf::String {})}}}}));
-    CHECK (vtfs ("(ssmas)") == opt (VariantType {{VT::Tuple {{basict (Leaf::String {}), basict (Leaf::String {}), VariantType {{VT::Maybe {{VariantType {{VT::Array {{basict (Leaf::String {})}}}}}}}}}}}}));
+    CHECK (vtfs ("(s)") == opt (VariantType {{VT::Tuple {{stringt (Leaf::string_)}}}}));
+    CHECK (vtfs ("(ss)") == opt (VariantType {{VT::Tuple {{stringt (Leaf::string_), stringt (Leaf::string_)}}}}));
+    CHECK (vtfs ("(ssmas)") == opt (VariantType {{VT::Tuple {{stringt (Leaf::string_), stringt (Leaf::string_), VariantType {{VT::Maybe {{VariantType {{VT::Array {{stringt (Leaf::string_)}}}}}}}}}}}}));
 
     CHECK (vtfs ("(") == none_t);
     CHECK (vtfs ("(}") == none_t);
@@ -122,10 +130,10 @@ TEST_CASE ("Variant types are parsed", "[variant-types]")
 
   SECTION ("entry types")
   {
-    CHECK (vtfs ("{ss}") == opt (VariantType {{VT::Entry {{{Leaf::String {}}}, {basict (Leaf::String {})}}}}));
-    CHECK (vtfs ("{xmas}") == opt (VariantType {{VT::Entry {{{Leaf::I64 {}}}, {VariantType {{VT::Maybe {{VariantType {{VT::Array {{basict (Leaf::String {})}}}}}}}}}}}}));
-    CHECK (vtfs ("{?*}") == opt (VariantType {{VT::Entry {{{Leaf::AnyBasic {}}}, {VariantType {{Leaf::AnyType {}}}}}}}));
-    CHECK (vtfs ("a{?*}") == opt (VariantType {{VT::Array {{VariantType {{VT::Entry {{{Leaf::AnyBasic {}}}, {VariantType {{Leaf::AnyType {}}}}}}}}}}}));
+    CHECK (vtfs ("{ss}") == opt (VariantType {{VT::Entry {{{Leaf::StringType{{Leaf::string_}}}}, {stringt (Leaf::string_)}}}}));
+    CHECK (vtfs ("{xmas}") == opt (VariantType {{VT::Entry {{{Leaf::Basic{{Leaf::i64}}}}, {VariantType {{VT::Maybe {{VariantType {{VT::Array {{stringt (Leaf::string_)}}}}}}}}}}}}));
+    CHECK (vtfs ("{?*}") == opt (VariantType {{VT::Entry {{{Leaf::any_basic}}, {VariantType {{Leaf::any_type}}}}}}));
+    CHECK (vtfs ("a{?*}") == opt (VariantType {{VT::Array {{VariantType {{VT::Entry {{{Leaf::any_basic}}, {VariantType {{Leaf::any_type}}}}}}}}}}));
 
     CHECK (vtfs ("{}") == none_t);
     CHECK (vtfs ("{s}") == none_t);
@@ -166,6 +174,13 @@ basicf (T const& t)
   return {Leaf::Basic {{t}}};
 }
 
+template <typename T>
+VariantFormat
+stringf (T const& t)
+{
+  return {Leaf::StringType {{t}}};
+}
+
 auto none_f {std::optional<VariantFormat> {}};
 
 } // anonymous namespace
@@ -174,28 +189,36 @@ TEST_CASE ("Variant formats are parsed", "[variant-formats]")
 {
   SECTION ("basic formats")
   {
-    CHECK (vffs ("b") == opt (basicf (Leaf::Bool {})));
-    CHECK (vffs ("y") == opt (basicf (Leaf::Byte {})));
-    CHECK (vffs ("n") == opt (basicf (Leaf::I16 {})));
-    CHECK (vffs ("q") == opt (basicf (Leaf::U16 {})));
-    CHECK (vffs ("i") == opt (basicf (Leaf::I32 {})));
-    CHECK (vffs ("u") == opt (basicf (Leaf::U32 {})));
-    CHECK (vffs ("x") == opt (basicf (Leaf::I64 {})));
-    CHECK (vffs ("t") == opt (basicf (Leaf::U64 {})));
-    CHECK (vffs ("h") == opt (basicf (Leaf::Handle {})));
-    CHECK (vffs ("d") == opt (basicf (Leaf::Double {})));
-    CHECK (vffs ("s") == opt (basicf (Leaf::String {})));
-    CHECK (vffs ("o") == opt (basicf (Leaf::ObjectPath {})));
-    CHECK (vffs ("g") == opt (basicf (Leaf::Signature {})));
-    CHECK (vffs ("?") == opt (basicf (Leaf::AnyBasic {})));
+    CHECK (vffs ("b") == opt (basicf (Leaf::bool_)));
+    CHECK (vffs ("y") == opt (basicf (Leaf::byte_)));
+    CHECK (vffs ("n") == opt (basicf (Leaf::i16)));
+    CHECK (vffs ("q") == opt (basicf (Leaf::u16)));
+    CHECK (vffs ("i") == opt (basicf (Leaf::i32)));
+    CHECK (vffs ("u") == opt (basicf (Leaf::u32)));
+    CHECK (vffs ("x") == opt (basicf (Leaf::i64)));
+    CHECK (vffs ("t") == opt (basicf (Leaf::u64)));
+    CHECK (vffs ("h") == opt (basicf (Leaf::handle)));
+    CHECK (vffs ("d") == opt (basicf (Leaf::double_)));
+  }
+
+  SECTION ("string type formats")
+  {
+    CHECK (vffs ("s") == opt (stringf (Leaf::string_)));
+    CHECK (vffs ("o") == opt (stringf (Leaf::object_path)));
+    CHECK (vffs ("g") == opt (stringf (Leaf::signature)));
 
     CHECK (vffs ("k") == none_f);
   }
 
+  SECTION ("variant formats")
+  {
+    CHECK (vffs ("v") == opt (VariantFormat {{Leaf::variant}}));
+  }
+
   SECTION ("array formats")
   {
-    CHECK (vffs ("as") == opt (VariantFormat {{VT::Array {{basict (Leaf::String {})}}}}));
-    CHECK (vffs ("aas") == opt (VariantFormat {{VT::Array {{VariantType {{VT::Array {{basict (Leaf::String {})}}}}}}}}));
+    CHECK (vffs ("as") == opt (VariantFormat {{VT::Array {{stringt (Leaf::string_)}}}}));
+    CHECK (vffs ("aas") == opt (VariantFormat {{VT::Array {{VariantType {{VT::Array {{stringt (Leaf::string_)}}}}}}}}));
 
     CHECK (vffs ("a") == none_f);
     CHECK (vffs ("aa") == none_f);
@@ -203,25 +226,25 @@ TEST_CASE ("Variant formats are parsed", "[variant-formats]")
 
   SECTION ("at variant type formats")
   {
-    CHECK (vffs ("v") == opt (VariantFormat {{VF::AtVariantType {{{Leaf::Variant {}}}}}}));
-    CHECK (vffs ("r") == opt (VariantFormat {{VF::AtVariantType {{{Leaf::AnyTuple {}}}}}}));
-    CHECK (vffs ("*") == opt (VariantFormat {{VF::AtVariantType {{{Leaf::AnyType {}}}}}}));
+    CHECK (vffs ("r") == opt (VariantFormat {{VF::AtVariantType {{{Leaf::any_tuple}}}}}));
+    CHECK (vffs ("*") == opt (VariantFormat {{VF::AtVariantType {{{Leaf::any_type}}}}}));
+    CHECK (vffs ("?") == opt (VariantFormat {{VF::AtVariantType {{{Leaf::any_basic}}}}}));
 
-    CHECK (vffs ("@s") == opt (VariantFormat {{VF::AtVariantType {basict (Leaf::String {})}}}));
+    CHECK (vffs ("@s") == opt (VariantFormat {{VF::AtVariantType {stringt (Leaf::string_)}}}));
+    CHECK (vffs ("@v") == opt (VariantFormat {{VF::AtVariantType {Leaf::variant}}}));
 
     CHECK (vffs ("@") == none_f);
 
     CHECK (vffs ("r") == vffs ("@r"));
     CHECK (vffs ("*") == vffs ("@*"));
     CHECK (vffs ("?") == vffs ("@?"));
-    CHECK (vffs ("v") == vffs ("@v"));
   }
 
   SECTION ("pointer formats")
   {
-    CHECK (vffs ("&s") == opt (VariantFormat {{VF::Pointer {{Leaf::String {}}}}}));
-    CHECK (vffs ("&o") == opt (VariantFormat {{VF::Pointer {{Leaf::ObjectPath {}}}}}));
-    CHECK (vffs ("&g") == opt (VariantFormat {{VF::Pointer {{Leaf::Signature {}}}}}));
+    CHECK (vffs ("&s") == opt (VariantFormat {{VF::Pointer {{Leaf::string_}}}}));
+    CHECK (vffs ("&o") == opt (VariantFormat {{VF::Pointer {{Leaf::object_path}}}}));
+    CHECK (vffs ("&g") == opt (VariantFormat {{VF::Pointer {{Leaf::signature}}}}));
 
     CHECK (vffs ("&i") == none_f);
     CHECK (vffs ("&ay") == none_f);
@@ -233,4 +256,3 @@ TEST_CASE ("Variant formats are parsed", "[variant-formats]")
   // TODO: Tuple formats
   // TODO: Entry formats
 }
-*/
