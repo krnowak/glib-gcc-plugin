87df471b5885ecb76becb893d67c6c59d985c810
Krzesimir Nowak <qdlacz@gmail.com>
2019-01-19 11:49:47 +0100

    foo

--- ggp/test/type-test.cc
+++ ggp/test/type-test.cc
@@ -95,6 +95,12 @@ variant_variant() -> std::vector<Types>
 }
 
 auto
+string_variant() -> std::vector<Types>
+{
+  return custom_variant (TypeInfo {{VariantType {{Leaf::StringType {{Leaf::string_}}}}}});
+}
+
+auto
 array_of_strings() -> std::vector<Types>
 {
   return {Types {{NullablePointer {{PlainType {{VariantTyped {"GVariantBuilder", {{VariantType {{VT::Array {VariantType {{Leaf::StringType {{Leaf::string_}}}}}}}}}}}}}}}, {{NullablePointer {{Pointer {{PlainType {{VariantTyped {"GVariantIter", {{VariantType {{VT::Array {VariantType {{Leaf::StringType {{Leaf::string_}}}}}}}}}}}}}}}}}}}};
@@ -160,6 +166,161 @@ convenience_amp_a_y () -> std::vector<Types>
   return {Types {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}, {NullablePointer {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}};
 }
 
+auto
+maybe_string_array () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{PlainType {{VariantTyped {"GVariantBuilder", {{VariantType {{VT::Array {VariantType {{Leaf::StringType {{Leaf::string_}}}}}}}}}}}}}}}, {{NullablePointer {{Pointer {{PlainType {{VariantTyped {"GVariantIter", {{VariantType {{VT::Array {VariantType {{Leaf::StringType {{Leaf::string_}}}}}}}}}}}}}}}}}}}};
+}
+
+auto
+maybe_string () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{PlainType {{type_gchar ()}}}}}}}, {NullablePointer {{Pointer {{PlainType {{type_gchar ()}}}}}}}}};
+}
+
+auto
+maybe_custom_variant(TypeInfo const &ti) -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{PlainType {{VariantTyped {"GVariant"s, ti}}}}}}, {NullablePointer {{Pointer {{PlainType {{VariantTyped {"GVariant"s, ti}}}}}}}}}};
+}
+
+auto
+maybe_unspecified_variant () -> std::vector<Types>
+{
+  return maybe_custom_variant (TypeInfo {{variant_type_unspecified}});
+}
+
+auto
+maybe_any_type_variant() -> std::vector<Types>
+{
+  return maybe_custom_variant (TypeInfo {{VariantType {{Leaf::any_type}}}});
+}
+
+auto
+maybe_any_basic_variant() -> std::vector<Types>
+{
+  return maybe_custom_variant (TypeInfo {{VariantType {{Leaf::any_basic}}}});
+}
+
+auto
+maybe_any_tuple_variant() -> std::vector<Types>
+{
+  return maybe_custom_variant (TypeInfo {{VariantType {{Leaf::any_tuple}}}});
+}
+
+auto
+maybe_variant_variant() -> std::vector<Types>
+{
+  return maybe_custom_variant (TypeInfo {{VariantType {{Leaf::variant}}}});
+}
+
+auto
+maybe_string_variant() -> std::vector<Types>
+{
+  return maybe_custom_variant (TypeInfo {{VariantType {{Leaf::StringType {{Leaf::string_}}}}}});
+}
+
+auto
+maybe_pointer_to_string () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{PlainType {{type_gchar ()}}}}}}}, {NullablePointer {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}};
+}
+
+auto
+maybe_convenience_a_amp_a_y () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}, {NullablePointer {{Pointer {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}}};
+}
+
+auto
+maybe_convenience_a_amp_o () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}, {NullablePointer {{Pointer {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}}};
+}
+
+auto
+maybe_convenience_a_amp_s () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}, {NullablePointer {{Pointer {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}}};
+}
+
+auto
+maybe_convenience_a_a_y () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}, {NullablePointer {{Pointer {{Pointer {{PlainType {{type_gchar ()}}}}}}}}}}};
+}
+
+auto
+maybe_convenience_a_s () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}, {NullablePointer {{Pointer {{Pointer {{PlainType {{type_gchar ()}}}}}}}}}}};
+}
+
+auto
+maybe_convenience_a_o () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}}, {NullablePointer {{Pointer {{Pointer {{PlainType {{type_gchar ()}}}}}}}}}}};
+}
+
+auto
+maybe_convenience_a_y () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{PlainType {{type_gchar ()}}}}}}}, {NullablePointer {{Pointer {{PlainType {{type_gchar ()}}}}}}}}};
+}
+
+auto
+maybe_convenience_amp_a_y () -> std::vector<Types>
+{
+  return {Types {{NullablePointer {{Const {{PlainType {{type_gchar ()}}}}}}}, {NullablePointer {{Pointer {{Const {{PlainType {{type_gchar ()}}}}}}}}}}};
+}
+
+auto
+special_maybe_bool_types () -> Types
+{
+  return {{{PlainType {{type_gboolean ()}}}}, {NullablePointer {{PlainType {{type_gboolean ()}}}}}};
+}
+
+auto
+maybe_integral_type (Integral const& i) -> std::vector<Types>
+{
+  return {special_maybe_bool_types (), Types {{{PlainType {{i}}}}, {{NullablePointer {PlainType {{i}}}}}}};
+}
+
+auto
+maybe_real_type (Real const& r) -> std::vector<Types>
+{
+  return {special_maybe_bool_types (), Types {{{PlainType {{r}}}}, {{NullablePointer {PlainType {{r}}}}}}};
+}
+
+auto
+maybe_any_basic_to_any_type_entry () -> std::vector<Types>
+{
+  auto any_basic_variant_types {any_basic_variant ()};
+  auto any_type_variant_types {any_type_variant ()};
+
+  return {special_maybe_bool_types (), any_basic_variant_types[0], any_type_variant_types[0]};
+}
+
+auto
+maybe_bool_bool_tuple () -> std::vector<Types>
+{
+  auto bool_types {integral_type (type_gboolean ())};
+  return {special_maybe_bool_types (), bool_types[0], bool_types[0]};
+}
+
+auto
+maybe_maybe_string () -> std::vector<Types>
+{
+  return {special_maybe_bool_types (), Types {{NullablePointer {{Const {{PlainType {{type_gchar ()}}}}}}}, {NullablePointer {{Pointer {{PlainType {{type_gchar ()}}}}}}}}};
+}
+
+auto maybe_maybe_bool () -> std::vector<Types>
+{
+  auto bool_types {integral_type (type_gboolean ())};
+
+  return {special_maybe_bool_types (), special_maybe_bool_types (), bool_types[0]};
+}
+
 } // anonymous namespace
 
 // TODO: See "gvariant format strings" in devhelp for information
@@ -201,6 +362,7 @@ TEST_CASE ("format to types", "[type]")
   SECTION ("at variant type formats")
   {
     CHECK (tfs ("@v") == variant_variant ());
+    CHECK (tfs ("@s") == string_variant ());
     CHECK (tfs ("r") == any_tuple_variant ());
     CHECK (tfs ("r") == tfs ("@r"));
     CHECK (tfs ("*") == any_type_variant ());
@@ -230,7 +392,36 @@ TEST_CASE ("format to types", "[type]")
 
   SECTION ("maybe formats")
   {
-    // TODO
+    CHECK (tfs ("mas") == maybe_string_array ());
+    CHECK (tfs ("ms") == maybe_string ());
+    CHECK (tfs ("mv") == maybe_unspecified_variant ());
+    CHECK (tfs ("m*") == maybe_any_type_variant ());
+    CHECK (tfs ("m@*") == tfs ("m*"));
+    CHECK (tfs ("m?") == maybe_any_basic_variant ());
+    CHECK (tfs ("m@?") == tfs ("m?"));
+    CHECK (tfs ("mr") == maybe_any_tuple_variant ());
+    CHECK (tfs ("m@r") == tfs ("mr"));
+    CHECK (tfs ("mv") == maybe_variant_variant ());
+    CHECK (tfs ("m@v") == tfs ("mv"));
+    CHECK (tfs ("ms") == maybe_string_variant ());
+    CHECK (tfs ("m@s") == tfs ("ms"));
+    CHECK (tfs ("m&s") == maybe_pointer_to_string ());
+    CHECK (tfs ("m&o") == maybe_pointer_to_string ());
+    CHECK (tfs ("m&g") == maybe_pointer_to_string ());
+    CHECK (tfs ("m^a&ay") == maybe_convenience_a_amp_a_y ());
+    CHECK (tfs ("m^a&o") == maybe_convenience_a_amp_o ());
+    CHECK (tfs ("m^a&s") == maybe_convenience_a_amp_s ());
+    CHECK (tfs ("m^aay") == maybe_convenience_a_a_y ());
+    CHECK (tfs ("m^as") == maybe_convenience_a_s ());
+    CHECK (tfs ("m^ao") == maybe_convenience_a_o ());
+    CHECK (tfs ("m^ay") == maybe_convenience_a_y ());
+    CHECK (tfs ("m^&ay") == maybe_convenience_amp_a_y ());
+    CHECK (tfs ("mb") == maybe_integral_type (type_gboolean ()));
+    CHECK (tfs ("md") == maybe_real_type (type_gdouble ()));
+    CHECK (tfs ("m{?*}") == maybe_any_basic_to_any_type_entry ());
+    CHECK (tfs ("m(bb)") == maybe_bool_bool_tuple ());
+    CHECK (tfs ("mms") == maybe_maybe_string ());
+    CHECK (tfs ("mmb") == maybe_maybe_bool ());
   }
 
   SECTION ("entry formats")
